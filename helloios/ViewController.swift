856efda3eb15e9fe67b6fa418e3c23ac6ad7037f
孙瑞宗 <20539050@qq.com>
2016-06-02 14:28:37 +0800

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by 孙瑞宗 on 16/6/2.
+//  Copyright © 2016年 孙瑞宗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
